fdb85c59f6a53495e22ddc341a68931d21f41638
Yang <1369771759@qq.com>
2022-05-25 11:27:26 +0800

    crm

---
+++ src/main/java/com/yang/crm/commons/Contants.java
@@ -0,0 +1,8 @@
+package com.yang.crm.commons;
+
+public class Contants {
+    public static final String RETURN_OBJECT_CODE_SUCCESS = "1";
+    public static final String RETURN_OBJECT_CODE_FIIL = "0";
+    //保存当前用户的key
+    public static final String SESSION_USER="sessionUser" ;
+}
